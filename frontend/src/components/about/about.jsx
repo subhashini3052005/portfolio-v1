f6a498e45bfa0150ebd5ef4aa0096331e615aaf2
subhashini3052005 <ssubhashini575@gmail.com>
2026-01-31 21:18:29 +0530

    about page ongoing

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
-import ic from "../../assets/icon.png"
+import { FaGithub } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { FaLinkedin } from 'react-icons/fa'
 
  const About = () => {
   return (
-    <section className='relative flex flex-col items-center bg-black min-h-screen overflow-hidden '>
+    <section className='relative flex flex-col items-center bg-[#02040f] min-h-screen pb-20 overflow-hidden '>
       <hr className='w-full h-[5px] relative rounded-full border-0 bg-[radial-gradient(circle_at_center,rgb(240,21,21),rgba(8,8,126,0.623))] mb-25'/>
-    <h1 className='text-white border rounded-full flex flex-row items-center gap-4 border-red-800 px-10 py-3 uppercase tracking-[0.2em] backdrop-blur bg-black/20 drop-shadow-[0_0_10px_rgba(220,38,38,1)] font-semibold '>About Me
-      <div className='w-3 h-3 bg-red-700 rounded-full animate-slowpulse'/>
+    <h1 className='text-cyan-100/80 border-cyan-500/20 bg-[#02040f]/60 drop-shadow-[0_0px_12px_rgba(34,211,238,0.6)] border rounded-full flex flex-row items-center gap-4 px-10 py-3 uppercase tracking-[0.2em] backdrop-blur  font-semibold '>About Me
+      <div className='w-3 h-3 bg-cyan-400/70 rounded-full animate-slowpulse'/>
     </h1> 
-    <h1 className='font-nosifer text-red-800 text-2xl'>know more</h1>
-    <div className='flex flex-row gap-30 mt-20'>
-    <div className='w-100 h-90 border border-red-700 shadow-[0_0_8px_#ef4444,0_0_16px_#ef4444] rounded-md'>
-    <img src={ic} alt="" />
+    <div className='flex w-full items-stretch flex-col max-w-7xl md:flex-row gap-30 mt-20'>
+
+    <div className='relative group hover:scale-105 transistion duration-300 text-cyan-200 w-full items-center px-10 py-12 max-w-3xl  border border-cyan-500/25 shadow-[0_0_40px_rgba(15,23,42,0.9),0_0_70px_rgba(34,211,238,0.15)] bg-[#02040f]/80 backdrop-blur rounded-md flex flex-col'>
+    <p className='uppercase tracking-[0.35em] text-xs text-cyan-400/40'>Subject Monitoring — Online</p>
+
+    <div className='w-36 h-36 flex items-center justify-center relative mt-5'>
+      <div className='w-30 h-30 flex items-center justify-center rounded-full bg-cyan-500/20 group-hover:scale-115 transistion duration-300'>
+        <svg className='w-20 h-20 text-cyan-300/80' viewBox='0 0 24 24' fill='currentcolor'>
+          <path d="M12 12a5 5 0 100-10 5 5 0 000 10zm0 2c-4.418 0-8 2.239-8 5v1h16v-1c0-2.761-3.582-5-8-5z"/>
+        </svg>
+      </div>
     </div>
-    <div className='w-100 h-90 border border-red-700 shadow-[0_0_8px_#ef4444,0_0_16px_#ef4444] rounded-md'>
+
+    <h1 className='uppercase tracking-wide font-bold text-3xl text-cyan-300 mt-5 '>subhashini</h1>
+      <p className='uppercase tracking-widest text-xs text-cyan-400/60 mt-3'>full stack developer</p>
+
+      <div className='mt-7 text-sm text-cyan-300/40 text-center font-mono space-y-1 '>
+        <p>&gt; initializing subject profile...</p>
+        <p>&gt; system integrity: stable</p>
+        <p>&gt; cognitive state: focused</p>
+      </div>
+
+      <div className='flex mt-8 gap-6 '>
+        <a href=""><FaLinkedin className='w-7 h-7 text-cyan-400/70 hover:text-cyan-200 hover:scale-110 transistion duration-300'/></a>
+        <a href=""><FaGithub className='w-7 h-7 text-cyan-400/70 hover:text-cyan-200 hover:scale-110 transistion duration-300'/></a>
+        <a href=""><HiOutlineMail className='w-7 h-7  text-cyan-400/70 hover:text-cyan-200 hover:scale-110 transistion duration-300'/></a>
+      </div>
+    </div>
+
+    <div className='relative text-cyan-200 w-full max-w-3xl h-90 border border-cyan-500/25 shadow-[0_0_40px_rgba(15,23,42,0.9),0_0_70px_rgba(34,211,238,0.15)] bg-[#02040f]/80 backdrop-blur rounded-md flex flex-col'>hii
     
     </div>
     </div>
