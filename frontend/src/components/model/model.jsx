45d38824c9d00c6d1ba16bdf7218abdc64c6f400
subhashini3052005 <ssubhashini575@gmail.com>
2026-02-14 23:31:47 +0530

    updated heropage

--- frontend/src/components/model/model.jsx
+++ frontend/src/components/model/model.jsx
@@ -2,16 +2,31 @@ import React from 'react'
 import Demo from '../../../public/Scene';
 import { Environment,OrbitControls } from '@react-three/drei';
 import { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useThree } from '@react-three/fiber';
+
+const Responsive = () =>{
+  const { size } = useThree()
+  const ismobile = size.width < 768
+  
+  return(
+    <Demo position={[ismobile ? -5 : 8,-10,0]}
+    scale = {ismobile ? 1.1 : 1}
+    />
+  )
+
+}
 
 const Model = () => {
   
   return (
-    <Canvas camera={{position:[80,20,70],fov:15}}>
-      <ambientLight/>
+    <Canvas className='w-full h-[80vh] md:h-screen' camera={{position : window.innerWidth < 768
+      ? [30,10,40]
+      : [80,20,70],
+      fov: window.innerWidth < 768 ? 35 : 15 }}>
+      <ambientLight intensity={1.2}/>
         <OrbitControls enableZoom={false}/>
         <Suspense fallback={null}>
-            <Demo position={[8,-10,0]}/>
+        <Responsive/>    
         </Suspense>
         <Environment preset='forest'/>
     </Canvas>
